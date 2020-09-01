74820c7cb0a213740b96bae5c1ff1c418a891d91
1noseA <1noseA2L@gmail.com>
2020-09-02 06:02:45 +0900

    [11-7]mainメソッド

--- Rectangle.java
+++ Rectangle.java
@@ -2,6 +2,14 @@ class Rectangle {
   int width;
   int height;
 
+  Rectangle() {
+    setSize(10, 20);
+  }
+
+  Rectangle(int w, int h) {
+    setSize(w, h);
+  }
+
   void setSize(int w, int h) {
     width = w;
     height = h;
@@ -9,5 +17,16 @@ class Rectangle {
 
   int getArea() {
     return width * height;
+  }
+
+  public static void main(String[] args) {
+    Rectangle r1 = new Rectangle();
+    System.out.println("r1.width = " + r1.width);
+    System.out.println("r1.height = " + r1.height);
+    System.out.println("r1.getArea() = " + r1.getArea());
+    Rectangle r2 = new Rectangle(123, 45);
+    System.out.println("r2.width = " + r2.width);
+    System.out.println("r2.height = " + r2.height);
+    System.out.println("r2.getArea() = " + r2.getArea());
   }
 }
